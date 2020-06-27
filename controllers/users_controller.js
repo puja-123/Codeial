f1625efd16ba02e4c6afe46a348c9b1af0967203
puja-123 <bhard.puja@gmail.com>
2020-06-28 03:04:28 +0530

    add session cookie

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,3 +4,25 @@ module.exports.profile = function(req,res)
         title : "Profile Page"
     } );
 }
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up' , {
+        title : "Codial | Sign Up "
+    })
+}
+//rendering the sign in page
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in' , {
+        title : "Codial | Sign In "
+    })
+}
+
+//get the sign up data
+module.exports.create = function(req,res)
+{
+    //TODO LATER
+}
+//get the sign in data
+module.exports.createSession = function(req,res)
+{
+    //TODO LATER
+}
